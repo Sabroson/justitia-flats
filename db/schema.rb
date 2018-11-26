7cdfea97f1111d567005d9f0a9922fb3eae1afd5
Jascha Drel <jaschadrel@gmail.com>
2018-11-26 18:41:01 +0100

    put the footer in the shared folder and moved the image slightly to the left

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,20 @@ ActiveRecord::Schema.define(version: 2018_11_26_164643) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "flats", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.text "description"
+    t.integer "price_per_night"
+    t.integer "number_of_guests"
+    t.bigint "user_id"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_flats_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -26,9 +40,10 @@ ActiveRecord::Schema.define(version: 2018_11_26_164643) do
     t.string "first_name"
     t.string "last_name"
     t.date "date_of_birth"
-    t.boolean "is_host"
+    t.boolean "is_host", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "flats", "users"
 end
