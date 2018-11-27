91263fcd0f2a3f1f7c9e4133c56b3956add532fa
Jascha Drel <jaschadrel@gmail.com>
2018-11-27 18:27:25 +0100

    created the flat controller including the addition of multiple pictures also the view for the form is adjusted

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -10,14 +10,35 @@ class FlatsController < ApplicationController
   def create
     @created_pictures = params["flat"]["pictures"]
     @flat = Flat.new(flat_params)
+    @flat.user = current_user
     if @flat.save
-      redirect_to flat_path(@flat)
+      if @created_pictures["picture"] || @created_pictures["picture0"]
+        @created_pictures.each do |key, value|
+          unless key.include? "cache"
+            create_picture(key, value)
+          end
+        end
+        set_primary_picture
+      end
+      redirect_to root_path
     else
       render :new
     end
   end
 
   private
+
+  def create_picture(key, value)
+    picture = Picture.new(url: value)
+    picture.flat = @flat
+    picture.save
+  end
+
+  def set_primary_picture
+    first_picture = @flat.pictures.first
+    first_picture.is_primary = true
+    first_picture.save
+  end
 
   def flat_params
     params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests, :pictures)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_27_120145) do
+ActiveRecord::Schema.define(version: 2018_11_27_111711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 2018_11_27_120145) do
     t.float "longitude"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "picture"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
@@ -50,7 +49,7 @@ ActiveRecord::Schema.define(version: 2018_11_27_120145) do
     t.string "first_name"
     t.string "last_name"
     t.date "date_of_birth"
-    t.boolean "is_host"
+    t.boolean "is_host", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
