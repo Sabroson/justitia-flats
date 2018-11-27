a26f8b6e65e1463190e2d2a7d2f147e0f78ba6fb
Daniel Mera de Agustin <daniel.meradeagustin@gmail.com>
2018-11-27 14:39:18 +0100

    add logout route and fix become a host route

--- db/schema.rb
+++ db/schema.rb
@@ -49,7 +49,7 @@ ActiveRecord::Schema.define(version: 2018_11_27_111711) do
     t.string "first_name"
     t.string "last_name"
     t.date "date_of_birth"
-    t.boolean "is_host", default: false
+    t.boolean "is_host"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
