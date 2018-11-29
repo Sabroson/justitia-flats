eb2b0d9f5761d7ad36f7bf038ea79e122a49a123
Jascha Drel <jaschadrel@gmail.com>
2018-11-29 12:16:14 +0100

    made sure that destroying an image from a flat works

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,5 +2,8 @@ class PicturesController < ApplicationController
   def destroy
     @picture = Picture.find(params[:id])
     Picture.destroy(@picture[:id])
+    respond_to do |format|
+        format.js
+      end
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,4 @@
 import "bootstrap";
 import "../components/flat_form";
 import "../components/autocomplete";
-import "../components/edit_pictures";
 
