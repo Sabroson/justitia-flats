30fde9bfe28b12c30b0d92ec0fdd19faefe1c481
Jascha Drel <jaschadrel@gmail.com>
2018-11-29 15:43:51 +0100

    finished the edit page

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,9 +1,34 @@
 class PicturesController < ApplicationController
+  before_action :set_picture, only: [:make_primary, :destroy]
+
+  def make_primary
+    @flat = @picture.flat
+
+    @flat_pictures = @flat.pictures
+
+    @flat_pictures.each do |picture|
+      picture.is_primary = false
+      picture.save
+    end
+
+    @picture.is_primary = true
+    @picture.save
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
   def destroy
-    @picture = Picture.find(params[:id])
     Picture.destroy(@picture[:id])
     respond_to do |format|
-        format.js
-      end
+      format.js
+    end
+  end
+
+  private
+
+  def set_picture
+    @picture = Picture.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   end
 
   resources :pictures, only: [:destroy]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  patch "pictures/:id/make_primary", to: "pictures#make_primary", as: "make_primary"
+
 end
