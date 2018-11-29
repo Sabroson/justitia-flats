a155e2ace448c1121ab2b4115a311d5ceceafe80
Lamina Vedder <lvedder@mail.uni-mannheim.de>
2018-11-29 17:34:21 +0100

    added a traveller overview of all bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -5,4 +5,10 @@ class Booking < ApplicationRecord
   belongs_to :flat
 
   validates :status, presence: true, inclusion: { in: BOOKING_STATUSES }
+
+ def primary_picture
+    primary = flat.pictures.where(is_primary: true).first
+    primary.nil? ? false : primary.url
+  end
+
 end
